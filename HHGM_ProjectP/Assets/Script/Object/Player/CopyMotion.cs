c4a49c4a94fcd00802782b79641e212a919461bf
ehgus9880822-admin <ehgus980822@gmail.com>
2024-05-29 16:05:23 +0900

    0529

--- HHGM_ProjectP/Assets/Script/Object/Player/CopyMotion.cs
+++ HHGM_ProjectP/Assets/Script/Object/Player/CopyMotion.cs
@@ -8,21 +8,62 @@ public class CopyMotion : MonoBehaviour
     public bool mirror;
 
     private ConfigurableJoint cj;
+    private Quaternion initialLocalRotation;
+    private Quaternion targetInitialLocalRotation;
 
     private void Awake()
     {
         cj = GetComponent<ConfigurableJoint>();
+        initialLocalRotation = transform.localRotation;
+        targetInitialLocalRotation = targetLimb.localRotation;
+    }
+
+    private void Start()
+    {
+        // JointDrive 설정을 통해 부드러운 움직임을 설정
+        JointDrive drive = new JointDrive();
+        drive.positionSpring = 500f; // 이 값을 조정하여 더 자연스러운 움직임을 얻습니다.
+        drive.positionDamper = 100f; // 이 값을 조정하여 감쇠를 제어합니다.
+        drive.maximumForce = 1000f;
+
+        cj.angularXDrive = drive;
+        cj.angularYZDrive = drive;
+
+        // 관절 회전 한계 설정
+        SoftJointLimit angularXLimit = new SoftJointLimit();
+        angularXLimit.limit = 45f;
+        cj.lowAngularXLimit = angularXLimit;
+        cj.highAngularXLimit = angularXLimit;
+
+        SoftJointLimit angularYZLimit = new SoftJointLimit();
+        angularYZLimit.limit = 45f;
+        cj.angularYLimit = angularYZLimit;
+        cj.angularZLimit = angularYZLimit;
+
+        // JointLimitSpring 설정
+        SoftJointLimitSpring angularLimitSpring = new SoftJointLimitSpring();
+        angularLimitSpring.spring = 500f;
+        angularLimitSpring.damper = 100f;
+
+        cj.angularXLimitSpring = angularLimitSpring;
+        cj.angularYZLimitSpring = angularLimitSpring;
     }
 
     private void LateUpdate()
     {
-        if (!mirror)
+        if (targetLimb != null)
         {
-            cj.targetRotation = targetLimb.rotation;
-        }
-        else
-        {
-            cj.targetRotation = targetLimb.rotation;
+            // targetLimb의 로컬 회전을 기준으로 targetRotation 계산
+            Quaternion targetRotation = Quaternion.Inverse(targetInitialLocalRotation) * targetLimb.localRotation;
+
+            if (mirror)
+            {
+                // 미러 모드에서 회전을 반대로 설정 (x축과 z축을 반전)
+                targetRotation = new Quaternion(-targetRotation.x, targetRotation.y, -targetRotation.z, targetRotation.w);
+            }
+
+            // 초기 로컬 회전을 고려하여 targetRotation 설정
+            cj.targetRotation = Quaternion.Inverse(initialLocalRotation) * targetRotation;
         }
     }
 }
---
+++ HHGM_ProjectP/Assets/Script/Object/Player/newcontrol.cs
@@ -0,0 +1,298 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newcontrol : MonoBehaviour
+{
+    public GameObject[] weapons;
+
+    public Transform weaponSocket;
+    public Transform player;
+    private Transform attachedWeapon;
+
+    public bool[] hasWeapons;
+    public float speed;
+    public float strafeSpeed;
+    public float jumpForce;
+    public bool isGround;
+    public Animator anim;
+    bool isJump;
+    public Rigidbody hip;
+    public bool faint = false;
+    public int nobanbok = 1;
+
+    public float HP = 100;
+
+
+    
+    
+
+    GameObject nearObject;
+
+
+    private void FixedUpdate()
+    {
+        // Forward Move
+        if (Input.GetKey(KeyCode.W) && (faint == false))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("isWalk", true);
+                anim.SetBool("isRun", true);
+
+                hip.AddForce(Vector3.forward * speed * 1.5f);
+            }
+            else
+            {
+                anim.SetBool("isWalk", true);
+                anim.SetBool("isRun", false);
+
+                hip.AddForce(Vector3.forward * speed);
+            }
+        }
+        else
+        {
+            anim.SetBool("isWalk", false);
+            anim.SetBool("isRun", false);
+        }
+
+        // Right Move
+        if (Input.GetKey(KeyCode.A) && (faint == false))
+        {
+            anim.SetBool("isSideLeft", true);
+            hip.AddForce(-Vector3.right * strafeSpeed);
+        }
+        else
+        {
+            anim.SetBool("isSideLeft", false);
+        }
+
+        // Back Move
+        if (Input.GetKey(KeyCode.S) && (faint == false))
+        {
+            anim.SetBool("isWalk", true);
+            hip.AddForce(-Vector3.forward * speed);
+        }
+        else if (!Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isWalk", false);
+        }
+
+        // Left Move
+        if (Input.GetKey(KeyCode.D) && (faint == false))
+        {
+            anim.SetBool("isSideRight", true);
+            hip.AddForce(Vector3.right * strafeSpeed);
+        }
+        else
+        {
+            anim.SetBool("isSideRight", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && (faint == false) && isGround)
+        {
+            hip.AddForce(new Vector3(0, jumpForce, 0));
+            isGround = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("isFilpkick", true);
+            hip.AddForce(Vector3.forward * speed * 5f);
+
+        }
+        else
+        {
+            anim.SetBool("isFilpkick", false);
+        }
+     
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (attachedWeapon == null) // 무기가 장착되어 있지 않은 경우
+            {
+                // 플레이어 주변의 모든 무기를 검사하여 가장 가까운 무기를 찾습니다.
+                Collider[] colliders = Physics.OverlapSphere(transform.position, 5f);
+                float closestDistance = Mathf.Infinity;
+                Collider closestCollider = null;
+
+                foreach (Collider col in colliders)
+                {
+                    if (col.CompareTag("Weapon"))
+                    {
+                        float distance = Vector3.Distance(transform.position, col.transform.position);
+                        if (distance < closestDistance)
+                        {
+                            closestDistance = distance;
+                            closestCollider = col;
+                        }
+                    }
+                }
+
+                // 가장 가까운 무기를 찾은 경우 해당 무기를 장착합니다.
+                if (closestCollider != null)
+                {
+                    AttachWeapon(closestCollider.transform);
+                }
+            }
+            else // 무기가 이미 장착되어 있는 경우
+            {
+                DetachWeapon();
+            }
+        }
+
+        if (HP <= 0)
+        {
+            if (nobanbok == 1)
+            {
+                gijal();
+            }
+        }
+
+
+
+        if (attachedWeapon != null && attachedWeapon.gameObject.name == "Bomb" && Input.GetMouseButtonDown(1)) // 폭탄이 장착되어 있고 우클릭을 눌렀을 때
+        {
+            ThrowBomb();
+        }
+        // 추가할 부분: "Dagger" 우클릭 감지 로직
+        if (attachedWeapon != null && attachedWeapon.gameObject.name == "Dagger" && Input.GetMouseButtonDown(1)) // 단검이 장착되어 있고 우클릭을 눌렀을 때
+        {
+            ThrowDagger();
+        }
+    }
+
+    void AttachWeapon(Transform weaponTransform)
+    {
+        // 무기의 Transform을 장착할 부위의 Transform에 할당합니다.
+        weaponTransform.parent = weaponSocket;
+        weaponTransform.localPosition = Vector3.zero;
+        weaponTransform.localRotation = Quaternion.identity;
+        weaponTransform.localScale = Vector3.one;
+
+        // 무기가 플레이어의 앞 방향을 바라보도록 회전합니다.
+        Vector3 direction = player.forward;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        weaponTransform.rotation = rotation;
+
+        // Rigidbody 비활성화
+        Rigidbody weaponRb = weaponTransform.GetComponent<Rigidbody>();
+        if (weaponRb != null)
+        {
+            weaponRb.isKinematic = true;
+            weaponRb.detectCollisions = false;
+        }
+
+        // 현재 장착된 무기를 저장합니다.
+        attachedWeapon = weaponTransform;
+
+        // 폭탄의 경우 4초 후에 자동으로 폭발하도록 설정
+        if (attachedWeapon.gameObject.name == "Bomb")
+        {
+            Invoke("AutoExplodeBomb", 4f);
+        }
+    }
+
+    void DetachWeapon()
+    {
+        if (attachedWeapon != null)
+        {
+            if (attachedWeapon.gameObject.name == "Bomb")
+            {
+                // 폭탄을 장착 해제하고 파괴합니다.
+                Destroy(attachedWeapon.gameObject);
+            }
+            else
+            {
+                // 다른 무기를 장착 해제합니다.
+                attachedWeapon.parent = null;
+
+                // 무기의 위치를 장착 위치의 바로 아래로 설정합니다.
+                attachedWeapon.position = weaponSocket.position - new Vector3(0, 0.5f, 0);
+
+                // Rigidbody 활성화
+                Rigidbody weaponRb = attachedWeapon.GetComponent<Rigidbody>();
+                if (weaponRb != null)
+                {
+                    weaponRb.isKinematic = false;
+                    weaponRb.detectCollisions = true;
+                }
+            }
+
+            attachedWeapon = null;
+        }
+    }
+
+
+
+    void ThrowBomb()
+    {
+        // 폭탄을 생성하고 throwPoint 위치에 던집니다.
+        GameObject bomb = Instantiate(attachedWeapon.gameObject, weaponSocket.position, Quaternion.identity);
+        Rigidbody bombRb = bomb.GetComponent<Rigidbody>();
+
+        // Rigidbody 활성화
+        if (bombRb != null)
+        {
+            bombRb.isKinematic = false;
+            bombRb.detectCollisions = true;
+            bombRb.AddForce(player.forward * 10f, ForceMode.Impulse);
+        }
+
+        // 일정 시간 후에 폭탄을 폭발시킵니다.
+        BombExplosion explosion = bomb.AddComponent<BombExplosion>();
+        explosion.explosionRadius = 5f;
+        explosion.explosionForce = 10f;
+        explosion.ExplodeAfterSeconds(3f);
+
+        DetachWeapon();
+    }
+
+    void ThrowDagger()
+    {
+        GameObject dagger = Instantiate(attachedWeapon.gameObject, weaponSocket.position, Quaternion.identity);
+        Dagger daggerScript = dagger.GetComponent<Dagger>();
+
+        // 단검이 날아갈 방향은 플레이어의 앞 방향으로 설정합니다.
+        Vector3 targetDirection = player.forward;
+        daggerScript.Initialize(targetDirection);
+
+        // 현재 장착된 단검을 제거합니다.
+        Destroy(attachedWeapon.gameObject);
+        attachedWeapon = null;
+    }
+
+    // 추가할 메서드: AutoExplodeBomb
+    void AutoExplodeBomb()
+    {
+        if (attachedWeapon != null && attachedWeapon.gameObject.name == "Bomb")
+        {
+            BombExplosion explosion = attachedWeapon.gameObject.AddComponent<BombExplosion>();
+            explosion.explosionRadius = 5f;
+            explosion.explosionForce = 10f;
+            explosion.ExplodeAfterSeconds(0f); // 즉시 폭발
+            attachedWeapon = null;
+        }
+    }
+
+    private void gijal()
+    {
+        faint = true;
+
+        
+    }
+
+    private void getup()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Punch")
+        {
+            HP -= 25;
+        }
+    }
+}
